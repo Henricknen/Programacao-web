8511b9cc0d3cc6dc19380713701a67a44744cd2c
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-03-18 00:50:31 -0300

    layout inicial

---
+++ ecommerce/resources/views/exemplo.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('titulo')
+
+    Pagina de exemplo
+
+@endsection
+
+@section('conteudo')
+
+    <h1>exemplo do conteudo</h1>
+    <div class="alert alert-danger">Conteudo do alert</div>
+
+@endsection
